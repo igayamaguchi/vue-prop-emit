f30f8609b7d5cfbd4580d922d540d7eb82a52ce5
igayamaguchi <iganin2@gmail.com>
2019-09-08 04:03:09 +0900

    add example

---
+++ App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Child :count="count" @set-count="handleSetCount" />
+</template>
+
+<script>
+import Child from "./Child";
+export default {
+  components: { Child },
+  data() {
+    return { count: 0 }
+  },
+  methods: {
+    handleSetCount(newCount) {
+      this.count = newCount;
+    }
+  }
+};
+</script>
---
+++ Child.vue
@@ -0,0 +1,21 @@
+<template>
+  <button @click="countUp">
+    +
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Child",
+  props: { count: { type: Number, required: true } },
+  methods: {
+    countUp() {
+      this.$emit('set-count', this.count++)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
